1d3c595a2830ac39e94250a39851290944032b10
penkoad <penkoad@yahoo.fr>
2013-06-26 16:25:14 +0200

    Changing color of message from yellow to green

--- Scripts/ubucleaner.sh
+++ Scripts/ubucleaner.sh
@@ -8,26 +8,27 @@ LINUXPKG="linux-(image|headers|ubuntu-modules|restricted-modules)"
 METALINUXPKG="linux-(image|headers|restricted-modules)-(generic|i386|server|common|rt|xen)"
 OLDKERNELS=$(dpkg -l|awk '{print $2}'|grep -E $LINUXPKG |grep -vE $METALINUXPKG|grep -v $CURKERNEL)
 YELLOW="\033[1;33m"
+GREEN="\033[1;32m"
 RED="\033[0;31m"
 ENDCOLOR="\033[0m"
 
 if [ $USER != root ]; then
   echo -e $RED"Error: must be root"
-  echo -e $YELLOW"Exiting..."$ENDCOLOR
+  echo -e $GREEN"Exiting..."$ENDCOLOR
   exit 0
 fi
 
-echo -e $YELLOW"Cleaning apt cache..."$ENDCOLOR
+echo -e $GREEN"Cleaning apt cache..."$ENDCOLOR
 aptitude clean
 
-echo -e $YELLOW"Removing old config files..."$ENDCOLOR
+echo -e $GREEN"Removing old config files..."$ENDCOLOR
 sudo aptitude purge $OLDCONF
 
-echo -e $YELLOW"Removing old kernels..."$ENDCOLOR
+echo -e $GREEN"Removing old kernels..."$ENDCOLOR
 sudo aptitude purge $OLDKERNELS
 
-echo -e $YELLOW"Emptying every trashes..."$ENDCOLOR
+echo -e $GREEN"Emptying every trashes..."$ENDCOLOR
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 
-echo -e $YELLOW"Script Finished!"$ENDCOLOR
+echo -e $GREEN"Script Finished!"$ENDCOLOR
